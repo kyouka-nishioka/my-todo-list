d6f982a83cceccc877df38bd981630b326561328
kurumikan66 <kyoka3740jrhdbu77@outlook.jp>
2020-11-09 18:43:37 +0900

    1 file modified

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,45 +1,48 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  }
+// class InputTodo extends Component {
+//   state = {
+//     title: ""
+//   }
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
-  }
+//   handleChange = e => {
+//     this.setState({
+//       [e.target.name]: e.target.value
+//     });
+//   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    // console.log(this.state.title);
-    this.props.addTodo(this.state.title);
-    this.setState({
-      title: ""
-    });
-  }
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     // console.log(this.state.title);
+//     this.props.addTodo(this.state.title);
+//     this.setState({
+//       title: ""
+//     });
+//   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input 
-          type="text" 
-          className="input-text"
-          placeholder="Add Todo.." 
-          value={this.state.title}
-          name="title"
-          onChange={this.handleChange}
-        />
-        <button 
-          type="submit"
-          className="input-submit"
-        >
-          Submit
-        </button>
-      </form>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className="form-container">
+//         <input 
+//           type="text" 
+//           className="input-text"
+//           placeholder="Add Todo.." 
+//           value={this.state.title}
+//           name="title"
+//           onChange={this.handleChange}
+//         />
+//         <button 
+//           type="submit"
+//           className="input-submit"
+//         >
+//           Submit
+//         </button>
+//       </form>
+//     );
+//   }
+// }
 
-export default InputTodo;
+// export default InputTodo;
+
+
+import React
